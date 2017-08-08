6f11422f8da3852a6bd34097d43775fb4cc2b7de
Yasassri Ratnayake <ycrnet@gmail.com>
2017-08-08 13:42:06 +0530

    Improving the back end server

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RESTfulService
 Simple Rest Client Used for Testing
+
+## statusCodeService
+
+This service allows you a request a status code from the Backend. The status code service supports following,
+
+* Status code with a response Body and without a response body
+ 
+ e.g : 
+````
+ GET http://localhost:8080/RESTfulService/rest/statusCodeService/200?withbody=true
+````
--- src/main/java/com/mock/rest/StatusCodeService.java
+++ src/main/java/com/mock/rest/StatusCodeService.java
@@ -1,18 +1,28 @@
 package com.mock.rest;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.*;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/statusCodeService")
-public class StatusCodeService {
+@Path("/statusCodeService") public class StatusCodeService {
 
-    @GET
-    @Path("/codes/{statusCode}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response setStatusCode(@PathParam("statusCode") String code) {
-        return Response.status(Integer.parseInt(code)).entity("Status Sent.").build();
+    @GET @Path("/{statusCode}") @Produces(MediaType.TEXT_PLAIN) public Response setStatusCodeGet(
+            @PathParam("statusCode") String code, @QueryParam("withbody") Boolean withBody) {
+        String response = null;
+        if (withBody) {
+            response = code + " with Body as the respose!";
+        }
+        return Response.status(Integer.parseInt(code)).entity(response).build();
+    }
+
+    @HEAD @Path("/{statusCode}") @Produces(MediaType.TEXT_PLAIN) public Response setStatusCodeHead(
+            @PathParam("statusCode") String code) {
+        String response = null;
+        return Response.status(Integer.parseInt(code)).entity(response).build();
     }
 }
