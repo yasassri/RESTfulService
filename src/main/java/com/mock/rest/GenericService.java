8b416bfebefc1d645a5648929c9d6fc18291a597
Yasassri Ratnayake <ycrnet@gmail.com>
2017-08-11 11:12:26 +0530

    Adding a generic HTTP service

---
+++ src/main/java/com/mock/rest/GenericService.java
@@ -0,0 +1,40 @@
+package com.mock.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/service") public class GenericService {
+
+    static int requestCounter = 0;
+
+    @GET @Path("/get") public Response getMsg() {
+
+        String output = "Get Request Received";
+        return Response.status(200).entity(output).build();
+
+    }
+
+    @HEAD @Path("/head") public Response headMsg() {
+
+        String output = "Get Request Received";
+        return Response.status(200).entity(output).header("custom", "head request received").build();
+
+    }
+
+    @POST @Produces("application/json") @Path("/post") public Response postMsg() {
+        requestCounter++;
+        return Response.status(200).entity("{\" POST Method : Request Status\" : \"Sucess\"}").build();
+
+    }
+
+    @PUT @Produces("application/json") @Path("/put") public Response putMsg() {
+        requestCounter++;
+        return Response.status(200).entity("{\" PUT Method : Request Status\" : \"Sucess\"}").build();
+
+    }
+}
