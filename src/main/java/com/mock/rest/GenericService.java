0f41fe35b478691e78fab19454e72f9e8dc1ebdb
Yasassri Ratnayake <ycrnet@gmail.com>
2017-08-14 11:16:23 +0530

    Improving generic HTTP service

--- src/main/java/com/mock/rest/GenericService.java
+++ src/main/java/com/mock/rest/GenericService.java
@@ -10,31 +10,25 @@ import javax.ws.rs.core.Response;
 
 @Path("/service") public class GenericService {
 
-    static int requestCounter = 0;
-
     @GET @Path("/get") public Response getMsg() {
-
         String output = "Get Request Received";
         return Response.status(200).entity(output).build();
 
     }
 
     @HEAD @Path("/head") public Response headMsg() {
-
         String output = "Get Request Received";
         return Response.status(200).entity(output).header("custom", "head request received").build();
 
     }
 
     @POST @Produces("application/json") @Path("/post") public Response postMsg() {
-        requestCounter++;
-        return Response.status(200).entity("{\" POST Method : Request Status\" : \"Sucess\"}").build();
+        return Response.status(200).entity("{\"POST Method:Request Status\":\"Sucess\"}").build();
 
     }
 
     @PUT @Produces("application/json") @Path("/put") public Response putMsg() {
-        requestCounter++;
-        return Response.status(200).entity("{\" PUT Method : Request Status\" : \"Sucess\"}").build();
+        return Response.status(200).entity("{\"PUT Method:Request Status\":\"Sucess\"}").build();
 
     }
 }
