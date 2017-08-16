6ed10adf7c969bc928e5e1ead7a4ea7cc6a9cf63
Yasassri Ratnayake <ycrnet@gmail.com>
2017-08-16 11:29:17 +0530

    Adding more services

--- src/main/java/com/mock/rest/GenericService.java
+++ src/main/java/com/mock/rest/GenericService.java
@@ -1,5 +1,6 @@
 package com.mock.rest;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.HEAD;
 import javax.ws.rs.POST;
@@ -13,22 +14,34 @@ import javax.ws.rs.core.Response;
     @GET @Path("/get") public Response getMsg() {
         String output = "Get Request Received";
         return Response.status(200).entity(output).build();
-
     }
 
     @HEAD @Path("/head") public Response headMsg() {
         String output = "Get Request Received";
         return Response.status(200).entity(output).header("custom", "head request received").build();
-
     }
 
     @POST @Produces("application/json") @Path("/post") public Response postMsg() {
         return Response.status(200).entity("{\"POST Method:Request Status\":\"Sucess\"}").build();
-
     }
 
     @PUT @Produces("application/json") @Path("/put") public Response putMsg() {
         return Response.status(200).entity("{\"PUT Method:Request Status\":\"Sucess\"}").build();
+    }
 
+    @GET @Produces("application/json") @Path("/all") public Response allMsg() {
+        return Response.status(200).entity("{\"GET Method:Request Status\":\"Sucess\"}").build();
+    }
+
+    @PUT @Produces("application/json") @Path("/all") public Response allMsgPut() {
+        return Response.status(200).entity("{\"GET Method:Request Status\":\"Sucess\"}").build();
+    }
+
+    @POST @Produces("application/json") @Path("/all") public Response allMsgPost() {
+        return Response.status(200).entity("{\"POST Method:Request Status\":\"Sucess\"}").build();
+    }
+
+    @DELETE @Produces("application/json") @Path("/all") public Response allMsgDelete() {
+        return Response.status(200).entity("{\"DELETE Method:Request Status\":\"Sucess\"}").build();
     }
 }
